bfe2b9c213c2182734877630a1bb9d9f67930c85
GustavoDMS <pipe.gus2014@gmail.com>
2025-04-25 11:05:49 -0300

    updating docker setup

--- backend/app.py
+++ backend/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return {'teste': 'lionel'} , 500
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <div className="grid gap-16 justify-items-center items-center p-8 pb-20 min-h-screen sm:p-20 grid-rows-[20px_1fr_20px] font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col row-start-2 items-center sm:items-start gap-[32px]"></main>
+            <h1 className="text-4xl font-bold text-center">Ola lionel</h1>
             <footer className="flex flex-wrap row-start-3 justify-center items-center gap-[24px]">
                 <a
                     className="flex gap-2 items-center hover:underline hover:underline-offset-4"
@@ -35,6 +36,7 @@ export default function Home() {
                     />
                     Examples
                 </a>
+                <p> Hello Site</p>
                 <a
                     className="flex gap-2 items-center hover:underline hover:underline-offset-4"
                     href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
