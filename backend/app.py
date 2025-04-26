3322e49178128fb3fdc138559cb46c0fc1a6dfa4
GustavoDMS <pipe.gus2014@gmail.com>
2025-04-26 20:58:56 -0300

    showing db integration

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from os import environ
+import psycopg
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    NEXT_PUBLIC_TESTE=environ.get("NEXT_PUBLIC_TESTE")
-    return {'teste': NEXT_PUBLIC_TESTE} , 500
+    sql_result = ''
+    with psycopg.connect(environ.get("DATABASE_URL")) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM tb_paciente")
+            sql_result = cur.fetchall()
+    return {'teste': sql_result} , 500
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,17 @@
-export default function Home() {
+export default async function Home() {
+    const fds = await fetch('http://backend:4000/')
+    const data = await fds.json()
+    console.log(data)
+
     return (
         <div className="grid gap-16 justify-items-center items-center p-8 pb-20 min-h-screen sm:p-20 grid-rows-[20px_1fr_20px] font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col row-start-2 items-center sm:items-start gap-[32px]">
                 <h1 className="text-4xl font-bold text-center">
-                    {process.env.NEXT_PUBLIC_TESTE}
+                {data.teste.map((item,idx)=>{
+                    return (<p key={idx}>
+                    {item[1]}
+                    </p>)
+                               })}
                 </h1>
             </main>
         </div>
