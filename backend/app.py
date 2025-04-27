e3a6d28bb5be887f2f634f2040976a76e1d8606a
GustavoDMS <pipe.gus2014@gmail.com>
2025-04-26 21:19:20 -0300

    fix frontend eslint

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from os import environ
 import psycopg
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_geek():
-    sql_result = ''
+    sql_result = ""
     with psycopg.connect(environ.get("DATABASE_URL")) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM tb_paciente")
             sql_result = cur.fetchall()
-    return {'teste': sql_result} , 500
+    return {"teste": sql_result}, 500
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,18 @@
+export const dynamic = "force-dynamic";
 export default async function Home() {
-    const fds = await fetch('http://backend:4000/')
-    const data = await fds.json()
-    console.log(data)
+  const fds = await fetch("http://backend:4000/");
+  const data = await fds.json();
+  console.log(data);
 
-    return (
-        <div className="grid gap-16 justify-items-center items-center p-8 pb-20 min-h-screen sm:p-20 grid-rows-[20px_1fr_20px] font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col row-start-2 items-center sm:items-start gap-[32px]">
-                <h1 className="text-4xl font-bold text-center">
-                {data.teste.map((item,idx)=>{
-                    return (<p key={idx}>
-                    {item[1]}
-                    </p>)
-                               })}
-                </h1>
-            </main>
-        </div>
-    );
+  return (
+    <div className="grid gap-16 justify-items-center items-center p-8 pb-20 min-h-screen sm:p-20 grid-rows-[20px_1fr_20px] font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col row-start-2 items-center sm:items-start gap-[32px]">
+        <h1 className="text-4xl font-bold text-center">
+          {data.teste.map((item: any, idx: number) => {
+            return <p key={idx}>{item[1]}</p>;
+          })}
+        </h1>
+      </main>
+    </div>
+  );
 }
