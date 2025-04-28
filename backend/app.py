4c68abc68938b1779f1f8e31f9b5c1e7f4f91b49
GustavoDMS <pipe.gus2014@gmail.com>
2025-04-28 01:40:08 -0300

    open secret file

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,20 @@ import psycopg
 app = Flask(__name__)
 
 
+def manage_sensitive(name):
+    secret_fpath = f"/run/secrets/{name}"
+    if environ.get("FLASK_DEBUG") == "1":
+        return environ.get(name)
+
+    v2 = open(secret_fpath).read().rstrip("\n")
+    return v2
+
+
 @app.route("/")
 def hello_geek():
     sql_result = ""
-    with psycopg.connect(environ.get("DATABASE_URL")) as conn:
+    with psycopg.connect(manage_sensitive("DATABASE_URL")) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM tb_paciente")
             sql_result = cur.fetchall()
-    return {"teste": sql_result}, 500
+    return {"teste": sql_result}, 200
